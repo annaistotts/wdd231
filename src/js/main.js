94e1a0883c21a4b25e7c2d64ef13d2f7e2f80802
annaistotts <ins17001@byui.edu>
2025-04-25 13:34:26 -0600

    finished week 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
-import { getParkData } from "./parkService.mjs";
+import { getParkData } from './parkService.mjs';
 
 const parkData = getParkData();
+
+// Update disclaimer link
+const disclaimerLink = document.querySelector(".disclaimer a");
+disclaimerLink.href = parkData.url;
+disclaimerLink.innerHTML = parkData.fullName;
+
+// Update page title
+document.title = parkData.fullName;
+
+// Update hero image
+const heroImage = document.querySelector(".hero-banner > img");
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+
+// Template for hero text
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+}
+
+// Update hero banner text
+const heroContent = document.querySelector(".hero-banner__content");
+heroContent.innerHTML = parkInfoTemplate(parkData);
