eb3d85c6475e7b18bd0cfa0cc3a4ce78c9a76353
annaistotts <ins17001@byui.edu>
2025-05-23 19:54:36 -0500

    src W03 complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
-import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
-
-const parkData = getParkData();
 
 function setParkIntro(data) {
   const introEl = document.querySelector(".intro");
@@ -12,9 +10,17 @@ function setParkIntro(data) {
 function setParkInfoLinks(data) {
   const infoEl = document.querySelector(".info");
   const html = data.map(mediaCardTemplate).join("");
-  infoEl.insertAdjacentHTML("afterbegin", html);
+  infoEl.innerHTML = html;
 }
 
-setHeaderFooter(parkData);
-setParkIntro(parkData);
-setParkInfoLinks(parkInfoLinks);
+async function init() {
+  const parkData = await getParkData();
+  const links = getInfoLinks(parkData.images);
+
+  setHeaderFooter(parkData);
+  setParkIntro(parkData);
+  setParkInfoLinks(links);
+}
+
+init();
+
